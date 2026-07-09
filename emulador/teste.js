5d5cd5158326f984a556c0a5eb86c8e7ed9ab953
lapingoxada <joaom0049@gmail.com>
2026-07-08 21:45:03 -0300

    Update teste.js

--- emulador/teste.js
+++ emulador/teste.js
@@ -1,1 +1,7 @@
-alert("JavaScript funcionando!");
+const arquivo = document.getElementById("arquivoJogo");
+
+arquivo.addEventListener("change", function() {
+  if (arquivo.files.length > 0) {
+    alert("Arquivo selecionado: " + arquivo.files[0].name);
+  }
+});
